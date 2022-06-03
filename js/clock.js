db133d9ea0bc62173ad405f20371b828bda1e63c
luhluh-17 <castro.ralph17@gmail.com>
2022-06-04 05:13:43 +0800

    adds dynamic background

--- js/clock.js
+++ js/clock.js
@@ -5,7 +5,7 @@ export const displayTime = () => {
 
   hh = hh === 0 ? 12 : hh
   hh = hh > 12 ? hh - 12 : hh
-  hh = hh < 10 ? `0${hh}` : hh 
+  hh = hh < 10 ? `0${hh}` : hh
   mm = mm < 10 ? `0${mm}` : mm
 
   document.querySelector('[data-clock]').textContent = `${hh}:${mm}`
@@ -14,18 +14,27 @@ export const displayTime = () => {
 export const greetUser = () => {
   const date = new Date()
   let hh = date.getHours()
-
   let greet = ''
 
   if (hh >= 4 && hh <= 11) {
     greet = 'Good morning'
+    updateBackground('morning,sun')
   } else if (hh >= 12 && hh <= 16) {
     greet = 'Good afternoon'
+    updateBackground('afternoon')
   } else if (hh >= 17 && hh <= 24) {
     greet = 'Good evening'
+    updateBackground('dark nature')
   } else {
     greet = 'Hello'
+    updateBackground('abstract')
   }
 
   document.querySelector('[data-greeting]').textContent = greet
+}
+
+const updateBackground = (tags) => {
+  const bg = document.querySelector('[data-background]')
+  let link = `url('https://source.unsplash.com/1600x900/?${tags}')`
+  bg.style.backgroundImage = link
 }
